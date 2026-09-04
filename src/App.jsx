66d0d031940e3bd649e852ae2ae2809f292d867c
bhimsen <brkaran2k2@gmail.com>
2026-09-04 15:45:07 +0530

    Welcome.jsx file is added, App.jsx file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import Info from './components/Info'
-
+import Welcome from './components/Welcome'
 function App() {
   return (
     <>
     <h1> Application for CI & CD</h1>
     <Info />
+    <hr/>
+    <Welcome />
     </>
   );
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to my React App!</h1>
+      <p>This is a simple React application created with Vite.</p>
+    </div>
+  );
+}
